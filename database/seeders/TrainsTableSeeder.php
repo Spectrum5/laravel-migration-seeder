cd03c24b028c77e93d4805c5edcce2f9e6280992
Spectrum5 <toskusergio@gmail.com>
2023-03-15 00:59:27 +0100

    Completata milestone 3

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -21,20 +21,22 @@ class TrainsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        $newTrain = new Train;
-        $newTrain->company = $faker->company();
-        $newTrain->departure_station = $faker->city();
-        $newTrain->arrival_station = $faker->city();
-        while ($newTrain->departure_station == $newTrain->arrival_station){
+        for ($i = 0; $i < 10; $i++) {
+            $newTrain = new Train;
+            $newTrain->company = $faker->company();
+            $newTrain->departure_station = $faker->city();
             $newTrain->arrival_station = $faker->city();
-        };
-        $newTrain->departure_time = $faker->dateTimeBetween('-1 week', '+1 week');
-        $newTrain->arrival_time = $faker->dateTimeBetween('+1 week', '+2 week');
-        $newTrain->train_id = strtoupper($faker->bothify('??##??'));
-        $newTrain->carriages = $faker->numberBetween(2, 15);
-        $newTrain->on_time = $faker->boolean();
-        $newTrain->deleted = $faker->boolean();
+            while ($newTrain->departure_station == $newTrain->arrival_station) {
+                $newTrain->arrival_station = $faker->city();
+            };
+            $newTrain->departure_time = $faker->dateTimeBetween('-1 week', '+1 week');
+            $newTrain->arrival_time = $faker->dateTimeBetween('+1 week', '+2 week');
+            $newTrain->train_id = strtoupper($faker->bothify('??##??'));
+            $newTrain->carriages = $faker->numberBetween(2, 15);
+            $newTrain->on_time = $faker->boolean();
+            $newTrain->deleted = $faker->boolean();
 
-        $newTrain->save();
+            $newTrain->save();
+        }
     }
 }
