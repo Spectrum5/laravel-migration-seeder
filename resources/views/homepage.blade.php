a88401b269a5c4fe777d488f4c6fa3cde4a45e96
Spectrum5 <toskusergio@gmail.com>
2023-03-15 01:30:04 +0100

    Esercizio completato

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,18 +1,26 @@
 @extends('layout.main')
 
 @section('content')
-    @foreach ($trains as $train)
-    <div class="ticket">
-        <h1> {{$train->company}}</h1>
-        <div>
-            <p>Stazione di partenza: {{$train->departure_station}}</p>
-            <p>Stazione di arrivo: {{$train->arrival_station}}</p>
-        </div>
-        <div>
-            <p>Ticket valido il giorno: {{$train->departure_time}}</p>
-            <small>ID treno: {{$train->train_id}}</small>
-        </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="mx-auto d-flex flex-wrap justify-content-center">
+                @foreach ($trains as $train)
+                    <div class="col-md-4 d-flex justify-content-center">
+                        <div class="ticket">
+                            <h2> {{ $train->company }}</h2>
+                            <div>
+                                <p>Stazione di partenza: {{ $train->departure_station }}</p>
+                                <p>Stazione di arrivo: {{ $train->arrival_station }}</p>
+                            </div>
+                            <div>
+                                <p>Ticket valido il giorno: {{ $train->departure_time }}</p>
+                                <small>ID treno: {{ $train->train_id }}</small>
+                            </div>
 
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
     </div>
-    @endforeach
 @endsection
